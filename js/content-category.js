2157ee535bbd15aa1acd1eeaddf1067a0c99ab2f
CarolMontteiro <carolmonteiro1206@gmail.com>
2024-07-18 16:22:38 -0300

    Adicionando seção de categoria

---
+++ js/content-category.js
@@ -0,0 +1,34 @@
+$(".content-category").slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1200,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 494,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+// ACTIVE LINK
