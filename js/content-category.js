c24a4ec376fa7c458ba3fab6f385e0c224db451b
CarolMontteiro <carolmonteiro1206@gmail.com>
2024-10-08 08:07:18 -0300

    Finaizando carrossel slider de categoria com botões interativos

--- js/content-category.js
+++ js/content-category.js
@@ -1,41 +1,73 @@
-$(".content-category").slick({
-    dots: true,
-    infinite: false,
-    speed: 300,
-    slidesToShow: 4,
-    slidesToScroll: 4,
-    responsive: [
-        {
-            breakpoint: 1200,
-            settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
-                infinite: true,
-                dots: true
+// Inicialização do Slick Slider
+function initializeSlick(contentId) {
+    $("#" + contentId).slick({
+        dots: true,
+        infinite: false,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 494,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-        },
-        {
-            breakpoint: 992,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 2
-            }
-        },
-        {
-            breakpoint: 494,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
+        ]
+    });
+}
+
+const tabs = document.querySelectorAll('.tab-btn');
+
+tabs.forEach(tab => tab.addEventListener('click', () => tabClicked(tab)));
+
+function tabClicked(tab) {
+    const contents = document.querySelectorAll('.contentCategory');
+
+    tabs.forEach(tab => {
+        if ($(tab).hasClass('slick-initialized')) {
+            $(tab).slick('unslick');
         }
-    ]
+        tab.classList.remove('active');
+    });
+
+    tab.classList.add('active');
+
+    contents.forEach(contentCategory => {
+        if ($(contentCategory).hasClass('slick-initialized')) {
+            $(contentCategory).slick('unslick');
+        }
+        contentCategory.classList.remove('show');
+    });
+
+    const contentId = tab.getAttribute('content-id');
+
+    const contentCategory = document.getElementById(contentId);
+    contentCategory.classList.add('show');
+
+    initializeSlick(contentId);
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const initialContent = document.querySelector('.contentCategory.show');
+    if (initialContent) {
+        initializeSlick(initialContent.id);
+    }
 });
-
-// ACTIVE LINK
-
-// function btnService(category) {
-//
-//    const party = document.getElementById(category);
-//    party.classList.remove('content-category-inactive');
-//     // console.log(name);
-// }
